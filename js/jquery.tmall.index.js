642a821a251c94856b109917a96aa1de92ff0c0f
xiqian.lxq <505804914@qq.com>
2015-08-10 16:15:26 +0800

    倒数第二版
    1.懒加载+fadein效果
    2.改进幻灯片，但是仍然要改进

--- js/jquery.tmall.index.js
+++ js/jquery.tmall.index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("img").lazyload({placeholder:'../../images/icon_wait.gif'});
+	lazyload_cc("floor_loading",0);
 	changePic("#banner-women-clothes-div");//大幻灯片默认展示
 	$(document).scroll(function(){
 		/*固定的搜索栏*/
@@ -35,50 +35,9 @@ $(document).ready(function(){
 		$(this).css("padding-left","50px");
 
 	});
-	$(".floor-show-third ul li").mouseenter(function(){
-		$(this).children("img").clearQueue();
-		$(this).children("img").animate({padding:'0px'},300);
-	});
-	$(".floor-show-third ul li").mouseleave(function(){
-		$(this).children("img").clearQueue();
-		$(this).children("img").animate({padding:'0px 0px 0px 4px'},300);
-	});
 
 	$(".brand-rec-nav li").hover(function(){
 		$(this).toggleClass("selected");
-	});
-	/*小的滑动的幻灯片begin*/
-	$(".brand-slide-bg").children().bind('webkitAnimationStart', function() {
-       $(this).parent(".brand-slide-bg").attr("is-animating","true");     
-    });
-    $(".brand-slide-bg").children().bind('webkitAnimationEnd', function() {
-        $(this).removeClass("moveFromRight");
-        $(this).removeClass("moveFromLeft");
-        if($(this).hasClass("moveToLeft") || $(this).hasClass("moveToRight")){
-            $(this).removeClass("page-current");
-        } 
-        $(this).removeClass("moveToLeft");
-        $(this).removeClass("moveToRight");
-        $(this).parent(".brand-slide-bg").attr("is-animating","false");       
-    });
-	$(".arrow-right").click(function() {
-        slide_div=$(this).siblings(".brand-slide-content").children(".brand-slide-bg");
-        moveLittlePage(slide_div,"right","moveFromLeft","moveToRight");       
-    });
-    $(".arrow-left").click(function() {
-        slide_div=$(this).siblings(".brand-slide-content").children(".brand-slide-bg");
-        moveLittlePage(slide_div,"left","moveFromRight","moveToLeft");       
-    });
-	/*小的滑动的幻灯片end*/
-
-	$(".brand-slide-bg").bind('webkitTransitionEnd', function(){
-		index=$(this).attr("index");
-		if(index==4){
-			index=1;
-			$(this).css("-webkit-transition-duration","0s");
-			$(this).css("-webkit-transform","translate3d(-100px,0,0)");
-			$(this).attr("index",index);
-		}
 	});
 	autoMove();
 });
@@ -108,7 +67,7 @@ function changePic(div_id){
 	BCImg.animate({
 		width:'810px',//这里如果换成-=30px,那么我乱晃的时候就会有bug，why
 		height:'480px'
-	},2500,function(){console.info(div_id)});
+	},2500);
 	/*右边小图片进入*/
 	if(BCEm.length>0){
 		BCEm.stop();
@@ -117,25 +76,30 @@ function changePic(div_id){
 	}
 }
 function autoMove(){
-    moveLittlePage($(".brand-slide-bg").first(),"right","moveFromLeft","moveToRight");      
-    moveLittlePage($(".brand-slide-bg").last(),"right","moveFromLeft","moveToRight");     
-    autoTask=setTimeout("autoMove()",4000);
+	$(".brand-slide-bg").each(function(){
+		moveLittlePage($(this),"right","moveFromLeft","moveToRight");     
+	});
+	autoTask=setTimeout("autoMove()",4000);
 }
-function moveLittlePage(slide_div,way,from_css,to_css){
-	/*入参：slide_div 要滑动的元素所在的父元素
+/*入参：slide_div 要滑动的元素所在的父元素
 		   way： left或者right左边活着右边
 		   from_css:上一个元素怎么做
 		   to_css: 本元素怎么做
-	*/
-    if(slide_div.attr("is-animating")=="true"){return;}//动画未结束不能开始
+	  作用：左侧图片轮播效果
+*/
+function moveLittlePage(slide_div,way,from_css,to_css){
+    if(slide_div.attr("is-animating")=="true") {
+    	return;
+    	//动画未结束不能开始
+    }
     var current_css="page-current";
-    var current_p=slide_div.children(".page-current");
+    var current_p=slide_div.children(".page-current").first();
     current_p.addClass(to_css);
     if(way=="left"){
-        if (current_p.next().length<=0) {
+        if (current_p.next().length<=0) {        	
             slide_div.children().first().addClass(from_css);
             slide_div.children().first().addClass(current_css);
-        }else{
+        }else{        	
             current_p.next().addClass(from_css);
             current_p.next().addClass(current_css);
         }  
@@ -147,5 +111,81 @@ function moveLittlePage(slide_div,way,from_css,to_css){
             current_p.prev().addClass(from_css);
             current_p.prev().addClass(current_css);
         }  
-    }  
+    } 
+}
+/*懒加载小插件*/
+//功能：当div完整出现在屏幕时，加载。
+//参数className，需要进行懒加载的元素的类名，要取一样的名字
+//参数beginHeight，滚动条滚到哪里，开始监听
+//request-url属性，loading的图片自己准备。可以卸载.loading中。
+function lazyload_cc(className,beginHeight){
+	lazyDivList=$("."+className);
+	$(window).scroll(function(){
+		srcTop=$(window).scrollTop();
+		if(srcTop>=beginHeight){
+			lazyDivList.trigger("lazyme",$(window).scrollTop());
+		}				
+	});
+	lazyDivList.bind("lazyme",function(e,scrTop){
+		var offset=$(this).offset().top;
+		var interval=$(window).height()-$(this).height();//当前页面可视高度
+		var sumB=offset;
+		var sumS=offset-interval;
+		var url=$(this).attr("request-url");
+
+		if(scrTop>=sumS && scrTop<=sumB){
+			$(this).load(url,function(responseTxt,statusTxt,xhr){
+				if (statusTxt=="success") {
+					$(this).hide();
+					/**自定义函数begin**/
+					$(this).find(".arrow-left").click(function() {
+			        	slide_div=$(this).siblings(".brand-slide-content").children(".brand-slide-bg");
+			       	 	moveLittlePage(slide_div,"left","moveFromRight","moveToLeft");   
+			    	});
+			    	$(this).find(".arrow-right").click(function() {
+					        slide_div=$(this).siblings(".brand-slide-content").children(".brand-slide-bg");
+					        moveLittlePage(slide_div,"right","moveFromLeft","moveToRight");       
+					 });
+			    	$(this).find(".floor-show-third ul li").mouseenter(function(){
+						$(this).children("img").clearQueue();
+						$(this).children("img").animate({padding:'0px'},300);
+					});
+					$(this).find(".floor-show-third ul li").mouseleave(function(){
+						$(this).children("img").clearQueue();
+						$(this).children("img").animate({padding:'0px 0px 0px 4px'},300);
+					});
+					
+					/*小的滑动的幻灯片begin*/
+					var brandSlideBg=$(this).find(".brand-slide-bg");
+					brandSlideBg.children().bind('webkitAnimationStart', function() {
+				       brandSlideBg.attr("is-animating","true");     
+				    });
+				    brandSlideBg.children().bind('webkitAnimationEnd', function() {
+				        if($(this).hasClass("moveToLeft")){
+				        	$(this).removeClass("page-current");
+				    		$(this).removeClass("moveToLeft");
+				    	}
+				    	if($(this).hasClass("moveToRight")){
+				    		$(this).removeClass("page-current");
+				    		$(this).removeClass("moveToRight");
+				    	}
+						if($(this).hasClass("moveFromRight")){
+				    		$(this).addClass("page-current");
+				    		$(this).removeClass("moveFromRight");
+				    	}
+				        if($(this).hasClass("moveFromLeft")){
+
+				        	$(this).addClass("page-current");
+				    		$(this).removeClass("moveFromLeft");
+				    	}
+				        brandSlideBg.attr("is-animating","false"); 
+				    });
+				    /**自定义函数end**/
+				    $(this).fadeIn(500);
+					$(this).removeClass(className);
+					$(this).unbind("lazyme");
+				};
+			});
+		}
+	});
 }
